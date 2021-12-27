82f8b097219b250a42173ad15f83c2f67f2530c5
alexpaxton <thealexpaxton@gmail.com>
2021-12-27 11:49:18 -0800

    chore: add eslint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,18 @@
-import type { NextPage } from "next";
-import Head from "next/head";
+import Head from 'next/head'
+import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <Head>
-        <title>Sol Ring / MTG Deckbuilder</title>
-        <meta name="description" content="MTG Deck Builder" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div>
+            <Head>
+                <title>Sol Ring / MTG Deckbuilder</title>
+                <meta name="description" content="MTG Deck Builder" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <p>boosh</p>
-    </div>
-  );
-};
+            <p>boosh</p>
+        </div>
+    )
+}
 
-export default Home;
+export default Home
