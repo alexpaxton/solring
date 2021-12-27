977d4b39670d63075d3c4689f0b2a4bab36dc9dc
alexpaxton <thealexpaxton@gmail.com>
2021-12-27 15:52:41 -0800

    feat: setup prisma and planetscale

---
+++ pages/api/decks.ts
@@ -0,0 +1,21 @@
+import {prisma} from 'utils'
+
+export default async function assetHandler(req, res) {
+  const { method } = req
+
+  switch (method) {
+  case 'GET':
+    try {
+      const decks = await prisma.deck.findMany()
+      res.status(200).json(decks)
+    } catch (e) {
+      console.error('Request error', e)
+      res.status(500).json({ error: 'Error fetching decks' })
+    }
+    break
+  default:
+    res.setHeader('Allow', ['GET'])
+    res.status(405).end(`Method ${method} Not Allowed`)
+    break
+  }
+}
