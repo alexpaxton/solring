409ff8de13df3bb491dbb5611ae17b2bbc942c56
alexpaxton <thealexpaxton@gmail.com>
2021-12-27 17:26:11 -0800

    feat: build foundation of auth flow

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,35 @@
 import Head from 'next/head'
 import type { NextPage } from 'next'
 import {Test} from 'components/Test'
+import { useUser } from '@auth0/nextjs-auth0'
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <Head>
-        <title>Sol Ring / MTG Deckbuilder</title>
-        <meta name="description" content="MTG Deck Builder" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Test />
-    </div>
-  )
+  const { user, error, isLoading } = useUser()
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>{error.message}</div>
+
+  function createDeck() {
+    console.log(user?.sub)
+  }
+
+  if (user) {
+    return (
+      <div>
+        <Head>
+          <title>Sol Ring / MTG Deckbuilder</title>
+          <meta name="description" content="MTG Deck Builder" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <div>
+          Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+          <button onClick={createDeck}>Create Deck</button>
+        </div>
+        <Test />
+      </div>
+    )
+  }
+
+  return <a href="/api/auth/login">Login</a>
 }
 
 export default Home
