5cbc7d58ad55a7919ba22bcfdb0160b458d6e56c
alexpaxton <thealexpaxton@gmail.com>
2021-12-27 18:48:35 -0800

    feat: create appwrapper with navbar

---
+++ components/AppWrapper.tsx
@@ -0,0 +1,72 @@
+import { FC, ReactNode } from 'react'
+import styled from 'styled-components'
+import { useUser } from '@auth0/nextjs-auth0'
+
+interface Props {
+    children: ReactNode
+}
+
+export const AppWrapper: FC<Props> = ({children}) => {
+  const { user } = useUser()
+
+  let actions = (
+    <a href="/api/auth/login">Login</a>
+  )
+
+  if (user) {
+    actions = (
+      <>
+        <p>Welcome {user.name}!</p>
+        <a href="/api/auth/logout">Logout</a>
+      </>
+    )
+  }
+
+  return (
+    <App>
+      <Header>
+        <Logo>Sol Ring</Logo>
+        <Actions>{actions}</Actions>
+      </Header>
+      <Main>
+        {children}
+      </Main>
+    </App>
+  )
+}
+
+const App = styled.div`
+    width: 100vw;
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+`
+
+const Header = styled.header`
+    flex: 0 0 80px;
+    display: flex;
+    align-items: center;
+    background-color: #ccc;
+    padding: 0 30px;
+    justify-content: space-between;
+`
+
+const Logo = styled.h1`
+    font-size: 16px;
+    font-weight: bold;
+    margin: 0;
+    line-height: 1em;
+`
+
+const Actions = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+const Main = styled.main`
+    display: flex;
+`
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import 'normalize.css'
 import 'styles/globals.css'
 import type { AppProps } from 'next/app'
+import { AppWrapper } from 'components/AppWrapper'
 import { UserProvider } from '@auth0/nextjs-auth0'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
-      <Component {...pageProps} />
+      <AppWrapper>
+        <Component {...pageProps} />
+      </AppWrapper>
     </UserProvider>
   )
 }
