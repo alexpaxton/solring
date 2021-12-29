438d2ccbf40208ff0630d362f83a840e4cd1bdb7
alexpaxton <thealexpaxton@gmail.com>
2021-12-29 14:14:28 -0800

    fix: update user with no handle instead of trying to create a new one

--- pages/api/users/create.ts
+++ pages/api/users/create.ts
@@ -12,8 +12,11 @@ export default async function assetHandler(
     try {
       const {email, handle} = body
       const user = await prisma.user.findUnique({where: {email}})
-      if (user) {
+      if (user && user.handle !== '') {
         res.status(500).json({error: 'User has already chosen a handle'})
+      } else if (user && !user.handle) {
+        const updatedUser = await prisma.user.update({where: {email}, data: {handle}})
+        res.status(200).json({ updatedUser })
       } else {
         const newUser = await prisma.user.create({data: {email, handle}})
         res.status(200).json({ newUser })
