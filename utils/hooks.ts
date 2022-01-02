be3e1292415f8c8649643b4c18e4614584306d42
alexpaxton <thealexpaxton@gmail.com>
2022-01-02 15:17:37 -0800

    feat: add useOnClickOutside hook

--- utils/hooks.ts
+++ utils/hooks.ts
@@ -1,14 +1,17 @@
-import {useEffect, useState} from 'react'
-import {Me} from 'types'
-import {getMe} from 'utils'
+import {
+  RefObject,
+  useEffect, useState
+} from 'react'
+import { Me } from 'types'
+import { getMe } from 'utils'
 
 interface UseMe extends Me {
   forceRefresh: () => Promise<void>
 }
 
 export const useMe = (): UseMe => {
-  const [isLoading, setLoading] = useState<boolean>(true)
-  const [me, updateMe] = useState<Me>({ isLoading: true })
+  const [ isLoading, setLoading ] = useState<boolean>(true)
+  const [ me, updateMe ] = useState<Me>({ isLoading: true })
 
   useEffect(() => {
     getCurrentUser()
@@ -35,5 +38,37 @@ export const useMe = (): UseMe => {
     setLoading(false)
   }
 
-  return { ...me, isLoading, forceRefresh: getCurrentUser }
+  return {
+    ...me,
+    isLoading,
+    forceRefresh: getCurrentUser 
+  }
+}
+
+type UseOnClickOutside = (ref: RefObject<HTMLElement>, handler: (e: MouseEvent) => void) => void
+
+export const useOnClickOutside: UseOnClickOutside = (ref, handler) => {
+  useEffect(
+    () => {
+      const listener = (event: MouseEvent) => {
+        const target = event.target as HTMLElement
+        // Do nothing if clicking ref's element or descendent elements
+        if (!ref.current || ref.current.contains(target)) {
+          return
+        }
+        handler(event)
+      }
+      document.addEventListener('mousedown', listener)
+      return () => {
+        document.removeEventListener('mousedown', listener)
+      }
+    },
+    // Add ref and handler to effect dependencies
+    // It's worth noting that because passed in handler is a new ...
+    // ... function on every render that will cause this effect ...
+    // ... callback/cleanup to run every render. It's not a big deal ...
+    // ... but to optimize you can wrap handler in useCallback before ...
+    // ... passing it into this hook.
+    [ ref, handler ]
+  )
 }
