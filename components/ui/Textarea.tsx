ef1c22a70d569bac12b24f3ecea191cdf2e79e74
alexpaxton <thealexpaxton@gmail.com>
2022-01-27 10:18:24 -0800

    feat: add textarea component to kit

---
+++ components/ui/Textarea.tsx
@@ -0,0 +1,48 @@
+import * as colors from 'components/ui/colors'
+import styled from 'styled-components'
+
+export const Textarea = styled.textarea`
+  position: relative;
+  z-index: 1;
+  border-radius: 4px;
+  height: 32px;
+  background-color: ${colors.n0};
+  border: 2px solid ${colors.n2};
+  font-size: 14px;
+  line-height: 16px;
+  font-weight: 500;
+  padding: 16px;
+  outline: none;
+  color: ${colors.n6};
+  transition: border-color 0.25s ease, color 0.25s ease,
+    background-color 0.25s ease, box-shadow 0.25s ease;
+
+  &::placeholder {
+    color: ${colors.n3};
+    font-style: italic;
+    transition: color 0.25s ease;
+  }
+
+  &:hover {
+    border-color: ${colors.p1};
+    color: ${colors.n7};
+    z-index: 2;
+  }
+
+  &:hover::placeholder {
+    color: ${colors.n4};
+  }
+
+  &:focus {
+    z-index: 2;
+    border-color: ${colors.p1};
+    color: ${colors.n7};
+    background-color: ${colors.n0};
+    box-shadow: 0 0 7px 1px ${colors.p0}, 0 0 3px 0 ${colors.p1};
+    text-shadow: 0 0 4px ${colors.p1};
+  }
+
+  &:focus::placeholder {
+    color: ${colors.p3};
+  }
+`
