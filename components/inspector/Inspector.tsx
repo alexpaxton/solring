9cffeb8d2a714f354b5c2add76b0c2989f9957b4
alexpaxton <thealexpaxton@gmail.com>
2022-01-29 15:57:02 -0800

    feat: display card previews and actions in inspector

--- components/inspector/Inspector.tsx
+++ components/inspector/Inspector.tsx
@@ -1,13 +1,116 @@
-import { Modal } from 'components/ui'
+import { InspectorActions } from 'components/inspector/InspectorActions'
+import { InspectorCard } from 'components/inspector/InspectorCard'
+import { colors, Modal } from 'components/ui'
 import { useInspector } from 'contexts'
-import { FC } from 'react'
+import { FC, MouseEvent, useRef, useState } from 'react'
+import { Card } from 'scryfall-sdk'
+import styled from 'styled-components'
+
+// Assigning inspectedCard to a ref so that it will still be visible
+// while the modal transitions out
 
 export const Inspector: FC = () => {
   const { inspectedCard, dismissInspector } = useInspector()
+  const [cardFace, setCardFace] = useState<string>('front')
+  const cardRef = useRef<Card | null>(null)
+
+  if (inspectedCard) {
+    cardRef.current = inspectedCard
+  }
+
+  function handleClick(e: MouseEvent) {
+    e.stopPropagation()
+  }
 
   return (
-    <Modal isVisible={!!inspectedCard} onMaskClick={dismissInspector}>
-      <p onClick={dismissInspector}>{inspectedCard?.name}</p>
+    <Modal isVisible={!!inspectedCard} onDismiss={dismissInspector}>
+      <Container onClick={handleClick}>
+        <InspectorCard
+          card={cardRef.current}
+          cardFace={cardFace}
+          setCardFace={setCardFace}
+        />
+        <CardDetails>
+          <CardName onClick={dismissInspector}>
+            {cardRef.current?.name}
+          </CardName>
+          <OracleText cardFace={cardFace} card={cardRef.current} />
+          <FlavorText cardFace={cardFace} card={cardRef.current} />
+          <InspectorActions />
+        </CardDetails>
+      </Container>
     </Modal>
   )
 }
+
+interface OracleTextProps {
+  card: Card | null
+  cardFace: string
+}
+
+const OracleText: FC<OracleTextProps> = ({ card, cardFace }) => {
+  let text
+  if (card?.card_faces) {
+    text =
+      cardFace === 'front'
+        ? card.card_faces[0].oracle_text
+        : card.card_faces[1].oracle_text
+  } else {
+    text = card?.oracle_text
+  }
+
+  return text ? <OracleP>{text}</OracleP> : null
+}
+
+interface FlavorTextProps {
+  card: Card | null
+  cardFace: string
+}
+
+const FlavorText: FC<FlavorTextProps> = ({ card, cardFace }) => {
+  let text
+  if (card?.card_faces) {
+    text =
+      cardFace === 'front'
+        ? card.card_faces[1].flavor_text
+        : card.card_faces[0].flavor_text
+  } else {
+    text = card?.flavor_text
+  }
+
+  return text ? <FlavorP>{text}</FlavorP> : null
+}
+
+const Container = styled.div`
+  display: flex;
+`
+
+const CardName = styled.h6`
+  font-size: 22px;
+  font-weight: 300;
+  margin: 0 0 1.5em 0;
+`
+
+const OracleP = styled.p`
+  margin: 0 0 16px 0;
+  font-size: 16px;
+  line-height: 22px;
+  font-weight: 400;
+`
+
+const FlavorP = styled.p`
+  margin: 0 0 16px 0;
+  font-size: 13px;
+  line-height: 18px;
+  font-weight: 400;
+  font-style: italic;
+`
+
+const CardDetails = styled.div`
+  margin-left: 32px;
+  width: 400px;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  color: ${colors.n7};
+`
---
+++ components/inspector/InspectorCard.tsx
@@ -0,0 +1,94 @@
+import { colors } from 'components/ui'
+import { FC } from 'react'
+import { Card } from 'scryfall-sdk'
+import styled from 'styled-components'
+
+interface Props {
+  card: Card | null
+  cardFace: string
+  setCardFace: (face: string) => void
+}
+
+export const InspectorCard: FC<Props> = ({ card, cardFace, setCardFace }) => {
+  if (card === null) {
+    return null
+  }
+
+  if (card.card_faces) {
+    const frontImage = card?.card_faces[0].image_uris?.large
+    const backImage = card?.card_faces[1].image_uris?.large
+
+    return (
+      <CardContainer className={cardFace}>
+        <SmallCard
+          className="front"
+          style={{ backgroundImage: `url(${frontImage})` }}
+          onClick={() => setCardFace('front')}
+        />
+        <SmallCard
+          className="back"
+          style={{ backgroundImage: `url(${backImage})` }}
+          onClick={() => setCardFace('back')}
+        />
+      </CardContainer>
+    )
+  }
+
+  const img = card?.image_uris?.large
+
+  return (
+    <CardContainer>
+      <BigCard style={{ backgroundImage: `url(${img})` }} />
+    </CardContainer>
+  )
+}
+
+const CardContainer = styled.div`
+  width: 400px;
+  height: 600px;
+  position: relative;
+
+  .front {
+    top: 0;
+    left: 0;
+  }
+
+  .back {
+    top: 50px;
+    left: 50px;
+  }
+`
+
+const CardBase = styled.div`
+  position: relative;
+  background-size: cover;
+  background-position: center center;
+`
+
+const BigCard = styled(CardBase)`
+  width: 400px;
+  border-radius: ${Math.round(400 * 0.0628571)}px;
+  height: ${Math.round(400 * 1.39666)}px;
+`
+
+const SmallCard = styled(CardBase)`
+  border-radius: 22px;
+  border-radius: ${Math.round(350 * 0.0628571)}px;
+  position: absolute;
+  width: 350px;
+  height: ${Math.round(350 * 1.39666)}px;
+  border: 2px solid transparent;
+  transition: border-color 0.25s ease, box-shadow 0.25s ease;
+
+  .front &.front,
+  .back &.back {
+    z-index: 2;
+    border-color: ${colors.p2};
+    box-shadow: 0 0 4px ${colors.p1}, 0 0 8px ${colors.p0};
+  }
+
+  .front &.back,
+  .back &.front {
+    cursor: pointer;
+  }
+`
