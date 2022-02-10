21988f27d4b899ca772033b4a4b66e540e64d9a6
alexpaxton <thealexpaxton@gmail.com>
2022-02-10 11:34:07 -0800

    chore: delete unused api code

--- utils/apiHooks.ts
+++ utils/apiHooks.ts
@@ -29,31 +29,3 @@ export const useMe = () => {
     isError: !!error || !!data?.error,
   }
 }
-
-// Deprecated
-const cardSearchFetcher = async (url: string, query: string) =>
-  fetch(`${url}${query}`).then((res) => res.json())
-
-export const useCardSearch = (query: Record<string, string | number>) => {
-  const queryString = objectToString(query)
-  const { data, error } = useSWR(
-    ['/api/search/cards', queryString],
-    cardSearchFetcher,
-  )
-
-  return {
-    data,
-    isError: error,
-  }
-}
-
-function objectToString(obj: Record<string, string | number>): string {
-  const entries = Object.entries(obj)
-  const result: string[] = []
-  entries.forEach((entry, i) => {
-    const prefix = i === 0 ? '?' : '&'
-    result.push(`${prefix}${entry[0]}=${entry[1]}`)
-  })
-
-  return result.join('')
-}
